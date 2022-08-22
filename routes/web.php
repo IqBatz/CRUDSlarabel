a4835c9646123986ec02dcd878a53cd95d98f096
IqBatz <78571493+IqBatz@users.noreply.github.com>
2022-08-22 10:16:57 -0600

    PrimerForm

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/hi', function () {
     return view('hola');
 });
+
+Route::resource('post', PostController::class);
